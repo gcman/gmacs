dda54c592de37eb09e920f45c2098e8495651594
Gautam Manohar <gcmanohar001@gmail.com>
2018-09-18 16:13:13 -0400

    Install magit and reorganize graphical decluttering config section

--- init.el
+++ init.el
@@ -10,3 +10,20 @@
 	(package-install 'use-package))
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/gautam.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (magit yasnippet-snippets yaml-mode writeroom-mode which-key wc-mode use-package sublimity solarized-theme smooth-scrolling smart-mode-line powerline ox-hugo org-wc org-mime org-bullets olivetti mu4e-alert imenu-anywhere flyspell-lazy flycheck diminish company centered-window cdlatex auctex-lua auctex-latexmk adaptive-wrap)))
+ '(uniquify-after-kill-buffer-p t)
+ '(uniquify-buffer-name-style (quote post-forward) nil (uniquify))
+ '(uniquify-strip-common-suffix t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
