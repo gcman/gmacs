e81d5d81cbc3a65671feb7af80a18b217849d409
Gautam Manohar <gcmanohar001@gmail.com>
2018-09-23 15:25:39 -0400

    Moved package init to org config and added god mode

--- init.el
+++ init.el
@@ -1,13 +1,9 @@
-(require 'package)
-(setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
-(package-initialize)
 
-;; Bootstrap `use-package'
-(unless (package-installed-p 'use-package)
-	(package-refresh-contents)
-	(package-install 'use-package))
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/gautam.org"))
 (custom-set-variables
