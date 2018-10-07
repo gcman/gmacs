dda6bbeb1524d8c6a21d0755635166e89377c547
Gautam Manohar <gcmanohar001@gmail.com>
2018-10-07 13:39:52 -0400

    Added lots of snippets

--- init.el
+++ init.el
@@ -1,7 +1,3 @@
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
 ;; (package-initialize)
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/gautam.org"))
@@ -10,7 +6,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files '("~/.emacs.d/gautam.org"))
+ '(cdlatex-math-modify-prefix 59)
+ '(cdlatex-math-symbol-prefix 59)
+ '(org-latex-pdf-process
+   '("xelatex -interaction nonstopmode -output-directory %o %f" "xelatex -interaction nonstopmode -output-directory %o %f" "xelatex -interaction nonstopmode -output-directory %o %f"))
  '(package-selected-packages
    '(company-math pdf-tools latexdiff magit yasnippet-snippets yaml-mode writeroom-mode which-key wc-mode use-package sublimity solarized-theme smooth-scrolling smart-mode-line powerline ox-hugo org-wc org-mime org-bullets olivetti mu4e-alert imenu-anywhere flyspell-lazy flycheck diminish company centered-window cdlatex auctex-lua auctex-latexmk adaptive-wrap))
  '(pdf-view-midnight-colors '("#839496" . "#fdf6e3"))
@@ -22,4 +21,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(variable-pitch ((t (:family "Palatino Linotype")))))
