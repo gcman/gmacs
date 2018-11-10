83879d05c6fb2df6e35a293ddf357dbe8727ec89
Gautam Manohar <gcmanohar001@gmail.com>
2018-11-10 13:56:11 -0500

    New snippets

--- abbrevs.el
+++ abbrevs.el
@@ -2,12 +2,17 @@
 (define-abbrev-table 'org-mode-abbrev-table
   '(
     ("bg" "background" nil :count 0)
+    ("bij" "bijective" nil :count 0)
     ("diffeq" "differential equation" nil :count 0)
-    ("fdvs" "finite-dimensional vector space" nil :count 1)
-    ("iff" "if and only if" nil :count 0)
+    ("fd" "finite dimensional" nil :count 8)
+    ("fdvs" "finite-dimensional vector space" nil :count 7)
+    ("iff" "if and only if" nil :count 4)
+    ("inj" "injective" nil :count 1)
     ("lindep" "linearly dependent" nil :count 0)
-    ("linind" "linearly independent" nil :count 3)
-    ("st" "such that" nil :count 0)
-    ("vs" "vector space" nil :count 5)
+    ("linind" "linearly independent" nil :count 4)
+    ("lm" "linear map" nil :count 1)
+    ("st" "such that" nil :count 3)
+    ("surj" "surjective" nil :count 2)
+    ("vs" "vector space" nil :count 11)
    ))
 
