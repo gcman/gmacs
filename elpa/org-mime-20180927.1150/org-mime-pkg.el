434a6fa779f2130edb9baa1cac3645d6a768aa7e
Gautam Manohar <gcmanohar001@gmail.com>
2018-12-08 14:55:30 -0500

    Added elisp and packages, reworked keybinds and commands

---
+++ elpa/org-mime-20180927.1150/org-mime-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "org-mime" "20180927.1150" "org html export for text/html MIME emails" '((emacs "24.4") (cl-lib "0.5")) :commit "baa6cde22fe10232009f4652a1b06bb1d60727e3" :keywords '("mime" "mail" "email" "html") :authors '(("Eric Schulte")) :maintainer '("Chen Bin (redguardtoo)") :url "http://github.com/org-mime/org-mime")
--- init.el
+++ init.el
@@ -6,10 +6,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(TeX-view-program-list '(("Evince" ("evince --page-index=%(outpage) %o") "")) t)
+ '(TeX-view-program-list '(("Evince" ("evince --page-index=%(outpage) %o") "")))
  '(custom-safe-themes
    '("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default))
- '(org-agenda-files '("~/Documents/school/org/todo.org"))
  '(org-format-latex-header
    "\\documentclass{article}
 \\usepackage[usenames]{color}
@@ -50,8 +49,9 @@
  '(org-latex-pdf-process
    '("xelatex -interaction nonstopmode -output-directory %o %f" "xelatex -interaction nonstopmode -output-directory %o %f" "xelatex -interaction nonstopmode -output-directory %o %f"))
  '(org-startup-indented t)
+ '(org-tags-column -80)
  '(package-selected-packages
-   '(mu4e-conversation ido-grid-mode avy-flycheck avy-menu avy-zap ido-vertical-mode flx-ido smex rainbow-mode yasnippet-snippets yaml-mode writeroom-mode which-key wc-mode use-package sublimity solarized-theme smooth-scrolling smart-mode-line powerline pdf-tools ox-hugo org-wc org-mime org-bullets olivetti nadvice mu4e-alert magit latexdiff ivy imenu-anywhere god-mode flyspell-lazy flycheck dired-quick-sort diminish dashboard company-math centered-window cdlatex auto-package-update auctex-lua auctex-latexmk adaptive-wrap))
+   '(amx mu4e-conversation ido-grid-mode avy-flycheck avy-menu avy-zap ido-vertical-mode flx-ido smex rainbow-mode yasnippet-snippets yaml-mode writeroom-mode which-key wc-mode use-package sublimity solarized-theme smooth-scrolling smart-mode-line powerline pdf-tools ox-hugo org-wc org-mime org-bullets olivetti nadvice mu4e-alert magit latexdiff ivy imenu-anywhere god-mode flyspell-lazy flycheck dired-quick-sort diminish dashboard company-math centered-window cdlatex auto-package-update auctex-lua auctex-latexmk adaptive-wrap))
  '(pdf-view-midnight-colors '("#657b83" . "#fdf6e3"))
  '(preview-default-option-list
    '("displaymath" "floats" "graphics" "textmath" "sections" "footnotes" "showlabels"))
