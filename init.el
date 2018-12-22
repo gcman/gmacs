ac3785737c07a0ad0af136f3abdac4303627266e
Gautam Manohar <gcmanohar001@gmail.com>
2018-12-22 13:00:50 -0500

    Redoing some aspects of packages

--- init.el
+++ init.el
@@ -4,7 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(TeX-view-program-list '(("Evince" ("evince --page-index=%(outpage) %o") "")))
+ '(TeX-view-program-list '(("Evince" ("evince --page-index=%(outpage) %o") "")) t)
  '(custom-safe-themes
    '("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default))
  '(org-format-latex-header
