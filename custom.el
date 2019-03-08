8280a1bf886b7c462b52adf3496fbce9cf1be297
Gautam Manohar <gcmanohar001@gmail.com>
2019-03-08 14:54:13 -0500

    Switch from linum to nlinum for speed increase

--- custom.el
+++ custom.el
@@ -3,8 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(beacon-color "#b58900" t)
- '(beacon-push-mark 35 t)
+ '(beacon-color "#b58900")
+ '(beacon-push-mark 35)
  '(custom-safe-themes
    '("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default))
  '(flycheck-display-errors-delay 0.3)
@@ -15,6 +15,7 @@
      (ivy-switch-buffer . ivy--regex-plus)
      (t . ivy--regex-fuzzy)) t)
  '(ivy-use-virtual-buffers t)
+ '(minibuffer-prompt-properties '(read-only t cursor-intangible t face minibuffer-prompt))
  '(nms-settings-file "/home/gautam/.emacs.d/cache/network-security.data" t)
  '(org-agenda-files
    '("~/org/inbox.org" "~/org/gtd.org" "~/org/notes.org" "~/org/gcal.org"))
@@ -77,7 +78,7 @@
 \\addtolength{\\textheight}{-3cm}
 \\setlength{\\topmargin}{1.5cm}
 \\addtolength{\\topmargin}{-2.54cm}")
- '(org-gcal-dir "/home/gautam/.emacs.d/cache/" t)
+ '(org-gcal-dir "/home/gautam/.emacs.d/cache/")
  '(org-latex-compiler "xetex")
  '(org-latex-default-class "gm-notes")
  '(org-refile-targets
@@ -88,7 +89,7 @@
  '(org-todo-keywords
    '((sequence "TODO(t)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c)")))
  '(package-selected-packages
-   '(uniquify elpy delight aggressive-indent sdcv-mode yasnippet yaml-mode ws-butler which-key web-mode use-package unfill undo-tree sudo-edit solarized-theme smex smart-mode-line rjsx-mode rainbow-mode rainbow-delimiters py-autopep8 projectile pdf-tools ox-hugo org-wc org-mime org-gcal org-cliplink org-bullets notmuch nix-mode neotree multiple-cursors markdown-mode magit lua-mode lispy linum-relative ledger-mode keyfreq hungry-delete god-mode ghc flycheck flx f expand-region dtrt-indent dired-quick-sort diminish diff-hl default-text-scale counsel company browse-kill-ring beacon bbdb auctex async-test ag adaptive-wrap))
+   '(nlinum-relative uniquify elpy delight aggressive-indent sdcv-mode yasnippet yaml-mode ws-butler which-key web-mode use-package unfill undo-tree sudo-edit solarized-theme smex smart-mode-line rjsx-mode rainbow-mode rainbow-delimiters py-autopep8 projectile pdf-tools ox-hugo org-wc org-mime org-gcal org-cliplink org-bullets notmuch nix-mode neotree multiple-cursors markdown-mode magit lua-mode lispy linum-relative ledger-mode keyfreq hungry-delete god-mode ghc flycheck flx f expand-region dtrt-indent dired-quick-sort diminish diff-hl default-text-scale counsel company browse-kill-ring beacon bbdb auctex async-test ag adaptive-wrap))
  '(pdf-misc-print-programm "/usr/bin/gtklp")
  '(preview-default-preamble
    '("\\RequirePackage{gm-math}" "\\RequirePackage["
@@ -109,9 +110,9 @@
  '(uniquify-after-kill-buffer-p t)
  '(uniquify-buffer-name-style 'post-forward nil (uniquify))
  '(uniquify-strip-common-suffix t)
- '(web-mode-code-indent-offset 2 t)
- '(web-mode-css-indent-offset 2 t)
- '(web-mode-markup-indent-offset 2 t))
+ '(web-mode-code-indent-offset 2)
+ '(web-mode-css-indent-offset 2)
+ '(web-mode-markup-indent-offset 2))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
