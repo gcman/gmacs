96c99db9cd42e30b1bc089c9c7048bb32333ea61
Gautam Manohar <gcmanohar001@gmail.com>
2019-03-12 18:38:05 -0400

    Fix breaking tangle

--- custom.el
+++ custom.el
@@ -92,4 +92,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-'(variable-pitch ((t (:family "Palatino Linotype")))))
+ '(variable-pitch ((t (:family "Palatino Linotype")))))
