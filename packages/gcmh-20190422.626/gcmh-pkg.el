3d3ebe9928c42106a9a091dd9fe6143a91d3f88d
Gautam Manohar <gcmanohar001@gmail.com>
2019-05-07 11:39:27 -0400

    Add garbage collection monitor package

---
+++ packages/gcmh-20190422.626/gcmh-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "gcmh" "20190422.626" "the Garbage Collector Magic Hack" '((emacs "24")) :commit "81844fa43b9af8cc0882f8cd25f6bbfd5bb390c7" :keywords '("internal") :maintainer '(nil . "andrea_corallo@yahoo.it") :url "https://gitlab.com/koral/gcmh")
