084f82788fe60449df8459eb4e7074350b37d26d
Gautam Manohar <gcmanohar001@gmail.com>
2019-05-20 22:28:55 -0400

    Customize org-ref

--- custom.el
+++ custom.el
@@ -3,13 +3,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(beacon-color "#b58900" t)
+ '(beacon-color "#b58900")
  '(custom-safe-themes
    (quote
-    ("0598c6a29e13e7112cfbc2f523e31927ab7dce56ebb2016b567e1eff6dc1fd4f" default))))
+    ("0598c6a29e13e7112cfbc2f523e31927ab7dce56ebb2016b567e1eff6dc1fd4f" default)))
+ '(org-agenda-files
+   (quote
+    ("/home/gautam/org/inbox.org" "/home/gautam/org/todo.org" "/home/gautam/org/notes.org" "/home/gautam/org/gcal.org"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(avy-lead-face ((t (:inherit isearch-fail))))
+ '(avy-lead-face-0 ((t (:inherit org-todo :background "#002b36"))))
  '(variable-pitch ((t (:family "Source Sans Pro" :height 115)))))
