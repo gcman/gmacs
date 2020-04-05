570e9b35ff98bd771d01f650873ec034230d3dc3
Gautam Manohar <gcmanohar001@gmail.com>
2020-04-05 16:45:40 -0700

    Fix align snippet

--- math-snippets.el
+++ math-snippets.el
@@ -1,5 +1,6 @@
 (setq gm/math-global-snippets
-      '(("qqd" "\\qquad ")
+      '(("Tor" "\\Tor ")
+        ("qqd" "\\qquad ")
         ("qd" "\\quad ")
         ("zsum" "\\sum_{n\\in\\Z} ")
         ("spec" "\\Spec ")
