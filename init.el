bb92af4946cc7faf5dd7b6251832dc5bfa03a4fa
Gautam Manohar <gcmanohar001@gmail.com>
2020-12-01 17:23:08 -0800

    Update to emacs 27

--- init.el
+++ init.el
@@ -14,16 +14,14 @@
 
 (let ((file-name-handler-alist nil)
       (orgfile (expand-file-name "gmacs.org" user-emacs-directory))
-      (elfile (expand-file-name "gmacs.el" user-emacs-directory))
-      (elcfile (expand-file-name "gmacs.elc" user-emacs-directory)))
-  (if (file-exists-p elcfile)
-      (load-file elcfile)
-    (if (or (not (file-exists-p elfile))
-            (file-newer-than-file-p orgfile elfile))
-        (progn
-          (require 'org)
-          (org-babel-load-file orgfile))
-      (load-file elfile))))
+      (elfile (expand-file-name "gmacs.el" user-emacs-directory)))
+
+  (if (or (not (file-exists-p elfile))
+          (file-newer-than-file-p orgfile elfile))
+      (progn
+        (require 'org)
+        (org-babel-load-file orgfile))
+    (load-file elfile)))
 
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (when (file-exists-p custom-file) (load custom-file))
